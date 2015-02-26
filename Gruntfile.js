3a749912482494e04db45b408f1758392c51376e
Robert Mulder <rmulder@xmission.com>
2015-02-26 13:31:35 -0800

    save updated plugin code

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+/*
+ * grunt-bootstrap-json2less
+ * https://github.com/rmulder/bootstrap-json2less
+ *
+ * Copyright (c) 2015 Robert Mulder
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    bootstrap_json2less: {
+      default_options: {
+        options: {
+        },
+        files: {
+          'test/output/variables.less': 'test/input/config.json'
+        }
+      },
+      custom_options: {
+        options: {
+          banner: '// DO NOT EDIT! GENERATED AUTOMATICALLY with grunt-bootstrap-json2less'
+        },
+        files: {
+          'test/output/variables.less': 'test/input/config.json'
+        }
+      }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'bootstrap_json2less', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/bootstrap_json2less.js
@@ -0,0 +1,51 @@
+/*
+ * grunt-bootstrap-json2less
+ * https://github.com/rmulder/bootstrap-json2less
+ *
+ * Copyright (c) 2015 Robert Mulder
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('bootstrap_json2less', 'Convert a Bootstrap config.json file to variables.less', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      banner: '// DO NOT EDIT! GENERATED AUTOMATICALLY with grunt-bootstrap-json2less'
+    });
+
+    function removeExtension(file) {
+      var result = file.split(".");
+      return result = result[0];
+    }
+
+    this.files.forEach(function (file) {
+      // parse, eval and save less variables
+      file.src.forEach(function (src) {
+        var content = grunt.file.read(src);
+        var lessContent = "";
+
+        console.log('===== this is the output of var content: ', content);
+        if (content.vars) {
+          for (var key in content.vars) {
+            lessContent += "@" + removeExtension(key.replace(/\\/g, "_"))+ ': "'+key+'";\n';
+          }
+        }
+
+
+        var outputFile = file.dest;
+        if (options.banner) {
+          lessContent = [options.banner, lessContent].join('\n');
+        }
+        grunt.file.write(outputFile, lessContent);
+        grunt.log.ok('%s succesfully created!', outputFile);
+      });
+    });
+  });
+
+};
