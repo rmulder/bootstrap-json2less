c4e139e988e2677411474a85e6493365ff359db5
Robert Mulder <rmulder@xmission.com>
2015-02-26 18:22:14 -0800

    save updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'bootstrap_json2less', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'bootstrap_json2less:default_options', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
--- tasks/bootstrap_json2less.js
+++ tasks/bootstrap_json2less.js
@@ -15,6 +15,7 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask('bootstrap_json2less', 'Convert a Bootstrap config.json file to variables.less', function() {
     // Merge task-specific and/or target-specific options with these defaults.
+    var shell = require('shelljs');
     var options = this.options({
       varPrefix: '',// set to '@' if not included in config.json
       banner: '// DO NOT EDIT! GENERATED AUTOMATICALLY with grunt-bootstrap-json2less'
@@ -28,25 +29,57 @@ module.exports = function(grunt) {
     this.files.forEach(function (file) {
       // parse, eval and save less variables
       file.src.forEach(function (src) {
-        var contents = grunt.file.read(src);
-        var content = JSON.parse(contents);
-        var lessContent = "";
+        var key, search, replace, content, inputContent, lessContent, outputFile = file.dest;
+        //lessContent = grunt.file.read(outputFile);
+        //console.log('lessContent:', lessContent);
+        inputContent = grunt.file.read(src);
+        content = JSON.parse(inputContent);
 
         //console.log('===== this is the output of var content: ', content);
         if (content.vars) {
-          for (var key in content.vars) {
-            lessContent += options.varPrefix + removeExtension(key.replace(/\\/g, "_"))+ ': '+content.vars[key]+';\n';
+          for (key in content.vars) {
+            //lessContent += options.varPrefix + removeExtension(key.replace(/\\/g, "_"))+ ': "'+key+'";\n';
+            search = options.varPrefix + removeExtension(key.replace(/\\/g, "_")) + ':';
+            replace = search + ' ' + content.vars[key]+';\n';
+            //lessContent += options.varPrefix + removeExtension(key.replace(/\\/g, "_"))+ ': '+content.vars[key]+';\n';
+            console.log('key:', key, 'value:', content.vars[key]);
+
+            //^(\@brand-primary:)(\s+.+;)(.+)
+            shell.sed('-i', '^(\\'+key+':)(\\s+.+;)(.+)', '\\1'+content.vars[key]+'\\3', file.dest);
+/*
+            var re1 = /^(\@brand-primary:)(\s+.+;)/ig;
+            var re2 = /^(\@brand-primary:)(\s+.+;)/ig;
+            //    return new RegExp('^' + matcher + '$');
+            //         regex = new RegExp('^' + regex + '$');
+//      regex = new RegExp('\\b' +  '(' + value.split(' ').join('|') + ')', 'i');
+//  return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, '\\$1');
+//    var source = '(?:' + _.keys(map).join('|') + ')';
+
+
+            var expression1 = '^(\\', expression2 = ':)(\\s+.+;)';
+            //var regex = new RegExp(expression1 + key + expression2, 'gi');
+            var regex = new RegExp('^(\\' + key + ':)(\\s+.+;)','gi');
+
+            //var matches = lessContent.match(/'^\@('+key+':)(\s+.+;)'/gi);
+            var matches = lessContent.match(regex);
+            console.log('regex:', regex, 'matches:', matches);
+            if (matches !== null) {
+              console.log('matches:', matches);
+            }
+*/
+            //lessContent.replace(/'^\@('+key+':)(\s+.+;)'/ig, '');
           }
         }
 
-        var outputFile = file.dest;
-        if (options.banner) {
+        /*if (options.banner) {
+          lessContent = grunt.file.read(outputFile);
           lessContent = [options.banner, lessContent].join('\n');
-        }
-        grunt.file.write(outputFile, lessContent);
-        grunt.log.ok('%s succesfully created!', outputFile);
+          grunt.file.write(outputFile, lessContent);
+        } */
+        grunt.log.ok('%s succesfully updated!', outputFile);
       });
     });
+
   });
 
 };
