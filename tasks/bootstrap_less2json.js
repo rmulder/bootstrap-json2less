2f3c0141cd1d78b46dd35a451434f1c64e68c064
Robert Mulder <rmulder@xmission.com>
2015-06-17 13:05:41 -0700

    Save updates to bootstrap_less2json (code cleanup)

--- tasks/bootstrap_less2json.js
+++ tasks/bootstrap_less2json.js
@@ -12,7 +12,7 @@ var less = require('less');
 
 module.exports = function (grunt) {
 
-  grunt.registerMultiTask('bootstrap_less2json', 'Convert a Bootstrap config.json file to variables.less', function() {
+  grunt.registerMultiTask('bootstrap_less2json', 'Convert a Bootstrap variables.less file to config.json', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     var shell = require('shelljs');
     var lessVars = {};
@@ -31,47 +31,25 @@ module.exports = function (grunt) {
       // parse, eval and save less variables to config.json settings file
       var outputFile = file.dest, outputContent, jsonContent;
       outputContent = grunt.file.read(outputFile);
-      //console.log('outputContent:', outputContent);
       jsonContent = JSON.parse(outputContent);
       file.src.forEach(function (src) {
         var key, value, matches, index, line, content;
         content = grunt.file.read(src).split('\n');
-        //var regex1 = new RegExp(/^(\\@(.+):)(\\s+)(.+);(.+)?\\n/gi);
-        //var regex1 = new RegExp('(\\@(.+):)(\\s+)(.+);(.+)?\\n');
         var regex1 = new RegExp('^(\\@.+:)(\\s+)(.+);(.+)?\\n');
-        //console.log('regex1:', regex1);
 
         var re = /^(\@.+):(\s+)(.+);(.+)?/ig;
         for (index in content) {
           line = content[index];
-          //console.log('line:', line);
-          //$search = options.varPrefix + removeExtension(key.replace(/\\/g, "_"));
-
-          //var matches = line.search(/^(\\@.+:)(\\s+)(.+);(.+)?\\n/gi);
-          //console.log('search: ', line.search(regex1));
-          //console.log('matches:', matches);
           if ((matches = re.exec(line)) != null) {
             key = matches[1] || null;
             value = matches[3] || null;
             if (key && value) {
-              //console.log('key:', key, 'value:', value, 'jsonContent.vars[key]:', jsonContent.vars[key]);
               jsonContent.vars[""+key] = value;
             }
           }
         }
-        //if (jsonContent.vars && content) {
-/*
-          for (key in jsonContent.vars) {
-            $search = options.varPrefix + removeExtension(key.replace(/\\/g, "_"));
-            var regex1 = new RegExp('(\\' + $search + ':)(\\s+)(.+);(.+)?\\n');
-            console.log('$search:', $search, 'regex1:', regex1);
-            //shell.sed('-i', regex1, "$1$2" + jsonContent.vars[key] + ';' + "$4\n", file.dest);
-          }
-          */
-        //}
-
       });
-      //console.log('updated jsonContent:', jsonContent.vars);
+
       grunt.file.write(outputFile, JSON.stringify(jsonContent, null, 2));
       grunt.log.ok('%s succesfully created!', outputFile);
       if (options.dist) {
